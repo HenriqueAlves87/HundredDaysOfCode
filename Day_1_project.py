3ac66cb311e5bbd1a65d65c8f8eff2d9aa8d592c
Henrique Alves <66229216+HenriqueAlves87@users.noreply.github.com>
2023-07-31 11:37:24 -0300

    Create Day_1_project.py

---
+++ Day_1_project.py
@@ -0,0 +1,17 @@
+#Day 1 - 100 Days of Code by Angela Yu - Udemy
+# This is Day 1 project on the course. A Band Name Generator
+
+#Go to: https://replit.com/@appbrewery/band-name-generator-start?v=1
+#1. Create a greeting for your program.
+print("Welcome to Band Name Generator! ")
+
+#2. Ask the user for the city that they grew up in.
+city_name = input("What's the name of the city that you grew up in? \n")
+
+#3. Ask the user for the name of a pet.
+pet_name = input("What's the name of your pet? \n")
+
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be: " + city_name + " " + pet_name)
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
